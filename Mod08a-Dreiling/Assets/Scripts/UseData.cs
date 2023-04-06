2e60d0c1ddd8794589e5c3b34e85831f0dd709dd
DDreiling <124920637+DDreiling@users.noreply.github.com>
2023-04-06 13:46:58 -0500

    Mod8a-Dreiling

--- Mod08a-Dreiling/Assets/Scripts/UseData.cs
+++ Mod08a-Dreiling/Assets/Scripts/UseData.cs
@@ -8,47 +8,71 @@ public class UseData : MonoBehaviour
   * https://github.com/tikonen/blog/tree/master/csvreader
   * */
 
-    List<Dictionary<string, object>> data; 
+    List<Dictionary<string, object>> data;
     public GameObject myCube;//prefab
-    int cubeCount; //variable 
+    int rowCount; //variable 
 
     private float startDelay = 2.0f;
     private float timeInterval = 1.0f;
+    public object tempObj;
+    public float tempFloat;
 
     void Awake()
     {
 
-        data = CSVReader.Read("udata");//udata is the name of the csv file 
+        data = CSVReader.Read("TestCO2");//udata is the name of the csv file 
 
         for (var i = 0; i < data.Count; i++)
         {
             //name, age, speed, description, is the headers of the database
             print("xco2 " + data[i]["xco2"] + " ");
-                   
+
         }
 
+        rowCount = 0;
 
     }//end Awake()
 
     // Use this for initialization
     void Start()
-    {
-        for (var i = 0; i < data.Count; i++)
-        {
-            object xco2 = data[i]["xco2"];//get age data
-            gameObject.transform.localScale = new Vector3 ( ( float) xco2,(float) xco2,(float) xco2) ;
-          
-            //cubeCount += (int)xco2;//convert age data to int and add to cubeCount
-            //Debug.Log("cubeCount " +cubeCount);
-        }
 
-        //InvokeRepeating("SpawnObject", startDelay, timeInterval);
+
+    {
+
+        InvokeRepeating("SpawnObject", startDelay, timeInterval);
 
     }//end Start()
 
-    // Update is called once per frame
-    void Update()
+    void SpawnObject()
     {
+        tempObj = (data[rowCount]["xco2"]);
+        tempFloat = System.Convert.ToSingle(tempObj);
+        tempFloat = (tempFloat - 350.0f) * 5.0f;
+        rowCount++;
+
+        transform.localScale = new Vector3(tempFloat, tempFloat, tempFloat);
+
+        Debug.Log("the tempFloat is" + tempFloat);
+        Debug.Log("Count" + rowCount);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // Update is called once per frame
+    //void Update()
+   // {
         //As long as cube count is not zero, instantiate prefab
        // while (cubeCount > 0)
        // {
@@ -60,18 +84,5 @@ public class UseData : MonoBehaviour
 
     }//end Update()
 
-     //void SpawnObject()
-      //  {
-            //if (cubeCount> 0)
-         //   {
-         //       Instantiate(myCube);
-         //       cubeCount--;
-        //        Debug.Log("cubecount " + cubeCount); 
-        //    }
-       // }
-
-
-
-   }
 
 
